b8195de8f2ae2b91af350bdd3d86ad65a3256ba5
vinayaktripathie <vinayaktripathi@hotmail.com>
2023-03-15 01:52:29 +0530

    issues fixes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -11,13 +11,14 @@ import GraphicEqIcon from "@mui/icons-material/GraphicEq";
 import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
 import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
 import DeveloperModeIcon from "@mui/icons-material/DeveloperMode";
+import { SpeedDialIcon } from "@mui/material";
 
-export const logo = "https://i.ibb.co/s9Qys2j/logo.png";
+export const logo = "https://i.ibb.co/Rcc6ftC/image.png";
 
 export const categories = [
   { name: "New", icon: <HomeIcon /> },
-  { name: "JS Mastery", icon: <CodeIcon /> },
-  { name: "Coding", icon: <CodeIcon /> },
+  { name: "Home", icon: <GraphicEqIcon /> },
+  { name: "Coding", icon: <SpeedDialIcon /> },
   { name: "ReactJS", icon: <CodeIcon /> },
   { name: "NextJS", icon: <CodeIcon /> },
   { name: "Music", icon: <MusicNoteIcon /> },
